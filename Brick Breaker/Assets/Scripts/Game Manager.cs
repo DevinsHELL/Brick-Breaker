666366491642b443f49c77b214d250a991b38a9e
DevinsHELL <000907680@mohawkcollege.ca>
2023-12-08 14:43:37 -0500

    Add unity project

---
+++ Brick Breaker/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int level = 1;
+    public int score = 0;
+    public int lives = 3;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        this.score = 0;
+        this.lives = 3;
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+        SceneManager.LoadScene("Level" + level);
+    }
+}
